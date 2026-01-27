1b4b01cb2959218e79f687410f1b09759b498987
App Territorios <territorios@example.com>
2026-01-26 22:29:47 -0500

    Fix: Downgrade Firebase to stable v11 and update all version strings to 2.3.9.2

--- scripts/force_bump.js
+++ scripts/force_bump.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2.3.8';
+const APP_VERSION = '2.3.9.2';
 const APP_NAME = 'Gestión de Territorios';
 
 // Note: This script is intended to be run in a browser console if possible, 
